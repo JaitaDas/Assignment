72a921d5d94ef8ff0ddb8f2bd69f441193a59458
JaitaDas <107671153+JaitaDas@users.noreply.github.com>
2022-06-28 09:39:00 +0530

    Update BloodDonation.java

--- BloodDonation.java
+++ BloodDonation.java
@@ -5,16 +5,16 @@ public class BloodDonation
   {
     int age,wgt;
 	Scanner sc=new Scanner(System.in);
-	System.out.print("Enter your age :");
+	System.out.print("Enter your age :");  //to print statement
 	age=sc.nextInt();
-	System.out.print("Enter your weight :");
+	System.out.print("Enter your weight :");  //to print statement
 	wgt=sc.nextInt();
 	if(age>=25)
 	{
 	  if(wgt>=48)
-	System.out.print("Eligible to donate blood");
+	System.out.print("Eligible to donate blood");   //to print statement
 	else
-	System.out.print("Not eligible to donate blood");
+	System.out.print("Not eligible to donate blood"); //to print statement
 	}
   }
 }
