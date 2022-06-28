745490107a93c15c92461a8c31ac0157fc35053b
JaitaDas <107671153+JaitaDas@users.noreply.github.com>
2022-06-28 09:31:28 +0530

    Update Vote.java

--- Vote.java
+++ Vote.java
@@ -4,12 +4,12 @@ public class Vote
   public static void main(String[] args)
   {
     int age;
-	Scanner sc=new Scanner(System.in);
-	System.out.print("Enter your age :");
+	Scanner sc=new Scanner(System.in);    //create scanner
+	System.out.print("Enter your age :");     //to print statement
 	age=sc.nextInt();
 	if(age>=18)
-	System.out.print("Eligible for vote");
+	System.out.print("Eligible for vote");   //to print a statement
 	else
-	System.out.print("Not eligible for vote");
+	System.out.print("Not eligible for vote");    //to print a statement
   }
 }
