c850cf6b0c4816f3db80c9ec8fab721b4282d349
JaitaDas <107671153+JaitaDas@users.noreply.github.com>
2022-06-28 09:37:00 +0530

    Update Number.java

--- Number.java
+++ Number.java
@@ -3,15 +3,15 @@ public class Number
 {
   public static void main(String[] args)
   {
-    int no;
+    int no;          //declear a variable 
 	Scanner sc=new Scanner(System.in);
 	System.out.print("Enter the number :");
 	no=sc.nextInt();
-	if(no>0)
+	if(no>0)   //check the number is greater than 0 or not
 	System.out.print("It is positive number");
-	else if(no<0)
+	else if(no<0)         //check the number is less than 0 or not
 	System.out.print("It is a negative number");
-	else
+	else  //execute when the above two conditions returns false
 	System.out.println("The number is 0");
   }
 }
